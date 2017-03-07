563b5a2ff714ea3b17549c22cebd6aef88a42ec7
Gavri Kepets <gkepets@gmail.com>
2017-03-06 20:26:08 -0500

    I think I added physics....

--- JetMovement.java
+++ JetMovement.java
@@ -12,19 +12,30 @@ import javax.swing.Timer;
 import java.lang.Math;
 
 public class JetMovement extends JPanel implements KeyListener, ActionListener{
+	double gravity = 9.87;
+	double velocity;
+	double velocityX;
+	double velocityY;
+
+	public double mass = 50.0;
+	public double thrust = 20000.0;
 	int time = 0;
 	JFrame w;
-	public player p;
+	public player p = new player();
 	public static JetMovement j;
 	public boolean left = false;
 	public boolean right = false;
 	public boolean forward = false;
 	public boolean game = true;
 	public double rotationSpeed = 1;
+	public double lastX;
+	public double lastY;
+
+
+
 	public JetMovement(){
 		Timer t = new Timer(10, this);
 		t.start();
-		p = new player();
 		p.xPos = 400;
 		p.yPos = 300;
 		setBackground(Color.RED);
@@ -42,24 +53,38 @@ public class JetMovement extends JPanel implements KeyListener, ActionListener{
   	//System.out.println(p.rotation+" degrees");
 	//System.out.print("Sin "+Math.sin(p.rotation));
 	//System.out.print("Cos "+Math.cos(p.rotation));
-  	//System.out.println("Runnning"); it works!
+  	//System.out.println("Runnning"); it works
+  	lastX = p.xPos;
+  	lastY = p.yPos;
+  	p.yPos +=  ((velocityY*0.01) + (0.0001* gravity * 0.5));
+  	p.xPos +=  ((velocityX*0.01));
 	if(right){
-		System.out.println("right");
+		//System.out.println("right");
 		p.rotation += rotationSpeed;
 	}
 	if(left){
-		System.out.println("left");
+		//System.out.println("left");
 		p.rotation -= rotationSpeed;
 	}
 	if(forward){
-		p.xPos += 1 * Math.sin(Math.toRadians(p.rotation));
-    	p.yPos += -1 * Math.cos(Math.toRadians(p.rotation)); 
+		p.addForce();
     	//System.out.println(p.rotation+" degrees");
-		//System.out.print("Sin "+Math.sin(Math.toRadians(p.rotation)));
+		System.out.print("Sin "+Math.sin(Math.toRadians(p.rotation)));
 		//System.out.print("Cos "+Math.cos(Math.toRadians(p.rotation)));		
 	}
+
+
+	findVelocity();
+	
+	System.out.println("Velocty: "+velocity);
     time++;
     repaint();
+  }
+
+  public void findVelocity(){
+  	velocity = (Math.sqrt((lastX-p.xPos)*(lastX-p.xPos) + (lastY-p.yPos)*(lastY-p.yPos)))/0.01;
+  	velocityX = (lastX-p.xPos)/0.01;
+  	velocityY = (lastY-p.yPos)/0.01;
   }
 
 
@@ -116,6 +141,10 @@ public class JetMovement extends JPanel implements KeyListener, ActionListener{
       		g2d.rotate(Math.toRadians(p.rotation),5, 10);  // Rotate the image..rotate(Math.toRadians(angle), m_imageWidth/2, m_imageHeight/2);
       		g2d.drawImage(img, 0, 0, 10, 20, j);
 			//g2d.draw(r);
+		}
+		public void addForce(){
+			p.xPos += 0.5*(velocity)*0.01 * Math.cos(Math.toRadians(p.rotation));
+    		p.yPos -= 0.5*(velocity)*0.01 * Math.cos(Math.toRadians(p.rotation)); 
 		}
 	}
 
