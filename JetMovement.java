2986fca23bd9d059d59310a3dfe9ee751e2698ae
Gavri Kepets <gkepets@gmail.com>
2017-03-08 13:01:56 -0500

    It Shoots With Physics Now

--- JetMovement.java
+++ JetMovement.java
@@ -10,193 +10,224 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
 import java.lang.Math;
-
-public class JetMovement extends JPanel implements KeyListener, ActionListener{
-	double gravity = 9.87;
-	double maxVelocity = 50.0;
-	double velocityX;
-	double velocityY;
- 	double accelerationY;
- 	double accelerationX;
- 	double forceY;
- 	double forceX;
- 	double inputForceY;
- 	double inputForceX;
-	public double mass = 1.0;
-	int time = 0;
-	JFrame w;
-	public player p = new player();
-	public static JetMovement j;
-	public boolean left = false;
-	public boolean right = false;
-	public boolean forward = false;
-	public boolean game = true;
-	public double rotationSpeed = 2;
-	public double lastX;
-	public double lastY;
+import java.awt.geom.AffineTransform;
 
 
+public class JetMovement extends JPanel implements KeyListener, ActionListener {
 
-	public JetMovement(){
-		Timer t = new Timer(10, this);
-		t.start();
-		p.xPos = 400;
-		p.yPos = 300;
-		setBackground(Color.RED);
-		w = new JFrame();
-		w.setSize(800,600);
-		w.setContentPane(this);
-		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		w.setVisible(true);
-		w.addKeyListener(this);
-	}
+    int time = 0;
+    JFrame w;
 
-
-	public void actionPerformed(ActionEvent e)
-  {
-  	//System.out.println(p.rotation+" degrees");
-	//System.out.print("Sin "+Math.sin(p.rotation));
-	//System.out.print("Cos "+Math.cos(p.rotation));
-  	//System.out.println("Runnning"); it works
- 	findAccelerationY();
-   	findVelocityY();
- 	findAccelerationX();
-   	findVelocityX();
-  	//System.out.println("position:"+ p.yPos + "  velocity: "+ velocityY);
-  	p.yPos += velocityY * 0.01;
-  	p.xPos +=  velocityX*0.01;
-	if(right){
-		//System.out.println("right");
-		p.rotation += rotationSpeed;
-	}
-	if(left){
-		//System.out.println("left");
-		p.rotation -= rotationSpeed;
-	}
-	if(forward){
-		forceX = 100*Math.sin(Math.toRadians(p.rotation));
-    	forceY = -100*Math.cos(Math.toRadians(p.rotation)); 
-	}
-	
+    double gravity = 9.87;
+    double maxVelocity = 50.0;
+    double velocityX;
+    double velocityY;
+    double accelerationY;
+    double accelerationX;
+    double forceY;
+    double forceX;
+    double inputForceY;
+    double inputForceX;
+    double mass = 1.0;
+    public player p = new player();
+    public static JetMovement j;
+    boolean left = false;
+    boolean right = false;
+    boolean forward = false;
+    boolean game = true;
+    double rotationSpeed = 2.5;
+    double lastX;
+    double lastY;
+    ArrayList<bullet> bullets = new ArrayList<bullet>();
 
 
-	
-//	/System.out.println("Velocty: "+velocity);
-    time++;
-    repaint();
-      System.out.println("(X,Y) | Acceleration "+accelerationX+", "+accelerationY+" | Velocity "+velocityX+", "+velocityY+" | Force "+forceX+", "+forceY);
-		System.out.println(velocityX);
-  }
+    public JetMovement() {
+        Timer t = new Timer(10, this);
+        t.start();
+        p.xPos = 400;
+        p.yPos = 300;
+        setBackground(Color.RED);
+        w = new JFrame();
+        w.setSize(800, 600);
+        w.setContentPane(this);
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setVisible(true);
+        w.addKeyListener(this);
+    }
 
 
-	
-
-	public void findVelocityY(){
-	if(forward){
-	if(Math.abs(velocityY) < maxVelocity){
-		velocityY += accelerationY * 0.01;
-	}
-	else if(velocityY > 0){
-		velocityY = maxVelocity-1;
-	}	
-	else if(velocityX < 0){
-		velocityY = (-maxVelocity)+1;
-	}	}
-else{
-	velocityY += accelerationY * 0.01;
-}
-}
-	public void findVelocityX(){
-	if(Math.abs(velocityX) < maxVelocity){
-		velocityX += accelerationX * 0.01;
-	}
-	else if(velocityX > 0){
-		velocityX = maxVelocity-1;
-	}	
-	else if(velocityX < 0){
-		velocityX = (-maxVelocity)+1;
-	}	
-	}
-
-  
-  public void findAccelerationY(){
-  	if(!forward){
-  		accelerationY = 9.8;
-  }
-  else{
-  	  	accelerationY = (forceY/mass);
-  }
-  }
-  public void findAccelerationX(){
-  	accelerationX = forceX/mass;
-  }
+    public void actionPerformed(ActionEvent e) {
+        for(bullet b : bullets){
+        b.xPos += 10*b.xRot;
+        b.yPos += 10* b.yRot;
+        }
+        //System.out.println(p.rotation+" degrees");
+        //System.out.print("Sin "+Math.sin(p.rotation));
+        //System.out.print("Cos "+Math.cos(p.rotation));
+        //System.out.println("Runnning"); it works
+        findAccelerationY();
+        findVelocityY();
+        findAccelerationX();
+        findVelocityX();
+        //System.out.println("position:"+ p.yPos + "  velocity: "+ velocityY);
+        p.yPos += velocityY * 0.01;
+        p.xPos += velocityX * 0.01;
+        if (right) {
+            //System.out.println("right");
+            p.rotation += rotationSpeed;
+        }
+        if (left) {
+            //System.out.println("left");
+            p.rotation -= rotationSpeed;
+        }
+        if (forward) {
+            forceX = 100 * Math.sin(Math.toRadians(p.rotation));
+            forceY = -100 * Math.cos(Math.toRadians(p.rotation));
+        }
 
 
- 	public void keyPressed(KeyEvent e) { 
- 		if(e.getKeyChar() == 'd'){
- 			//System.out.println("d pressed");
- 			right = true;
- 		}
- 		if(e.getKeyChar() == 'a'){
- 			//System.out.println("a pressed");
- 			left = true;
- 		}
- 		if(e.getKeyChar() == 'w'){
- 			//System.out.println("w pressed");
-			forward = true;
-		}
- 		
 
- 	}
-    public void keyReleased(KeyEvent e) { 
-    	if(e.getKeyChar() == 'd'){
- 			//System.out.println("d released");
- 			right = false;
- 		}
- 		if(e.getKeyChar() == 'a'){
- 			//System.out.println("a released");
- 			left = false;
- 		}
- 		if(e.getKeyChar() == 'w'){
- 			//System.out.println("w released");
-			forward = false;
-		}
+
+        //	/System.out.println("Velocty: "+velocity);
+        time++;
+        repaint();
+        //System.out.println("(X,Y) | Acceleration " + accelerationX + ", " + accelerationY + " | Velocity " + velocityX + ", " + velocityY + " | Force " + forceX + ", " + forceY);
+        //System.out.println(velocityX);
+    }
+
+
+
+
+    public void findVelocityY() {
+        if (forward) {
+            if (Math.abs(velocityY) < maxVelocity) {
+                velocityY += accelerationY * 0.01;
+            } else if (velocityY > 0) {
+                velocityY = maxVelocity - 1;
+            } else if (velocityX < 0) {
+                velocityY = (-maxVelocity) + 1;
+            }
+        } else {
+            velocityY += accelerationY * 0.01;
+        }
+    }
+    public void findVelocityX() {
+        if (Math.abs(velocityX) < maxVelocity) {
+            velocityX += accelerationX * 0.01;
+        } else if (velocityX > 0) {
+            velocityX = maxVelocity - 1;
+        } else if (velocityX < 0) {
+            velocityX = (-maxVelocity) + 1;
+        }
+    }
+
+
+    public void findAccelerationY() {
+        if (!forward) {
+            accelerationY = 9.8;
+        } else {
+            accelerationY = (forceY / mass);
+        }
+    }
+    public void findAccelerationX() {
+        accelerationX = forceX / mass;
+    }
+
+
+    public void shoot(){
+        bullet b = new bullet();
+        bullets.add(b);
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyChar() == 'd') {
+            //System.out.println("d pressed");
+            right = true;
+        }
+        if (e.getKeyChar() == 'a') {
+            //System.out.println("a pressed");
+            left = true;
+        }
+        if (e.getKeyChar() == 'w') {
+            //System.out.println("w pressed");
+            forward = true;
+        }
+        if(e.getKeyChar() == 's'){
+            System.out.println("Shoot");
+            shoot();
+        }
 
     }
-    public void keyTyped(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyChar() == 'd') {
+            //System.out.println("d released");
+            right = false;
+        }
+        if (e.getKeyChar() == 'a') {
+            //System.out.println("a released");
+            left = false;
+        }
+        if (e.getKeyChar() == 'w') {
+            //System.out.println("w released");
+            forward = false;
+        }
+
+
+    }
+    public void keyTyped(KeyEvent e) {}
 
 
 
-	class player{
-		double xPos;
-		double yPos;
-		double rotation;
-		Image img = new ImageIcon(this.getClass().getResource("player.png")).getImage();
+    class player {
+        double xPos;
+        double yPos;
+        double rotation;
+        Image img = new ImageIcon(this.getClass().getResource("player.png")).getImage();
 
 
-		public player(){
+        public player() {
 
 
-		}
-		public void draw(Graphics g){
-			Rectangle r = new Rectangle((int)xPos, (int)yPos, 20,20);
- 			Graphics2D g2d=(Graphics2D)g; // Create a Java2D version of g.
-      		g2d.translate(xPos, yPos); // Translate the center of our coordinates.
-      		g2d.rotate(Math.toRadians(p.rotation),5, 10);  // Rotate the image..rotate(Math.toRadians(angle), m_imageWidth/2, m_imageHeight/2);
-      		g2d.drawImage(img, 0, 0, 10, 20, j);
-			//g2d.draw(r);
-		}
-		
-	}
+        }
+        
+    }
+
+    class bullet {
+        double xPos;
+        double yPos;
+        double rotation;
+        double yRot;
+        double xRot;
+        Image img = new ImageIcon(this.getClass().getResource("player.png")).getImage();
+
+        public bullet(){
+            xPos = p.xPos;
+            yPos = p.yPos;
+            yRot = -Math.cos(Math.toRadians(p.rotation));
+            xRot = Math.sin(Math.toRadians(p.rotation));
+        }
+        
+
+    }
 
 
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		p.draw(g);
-	}
-	
-	public static void main(String[] args){
-		j = new JetMovement();
-	}
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g; // Create a Java2D version of g.
+        AffineTransform old = g2d.getTransform();
+        g2d.translate(p.xPos, p.yPos); // Translate the center of our coordinates.
+        g2d.rotate(Math.toRadians(p.rotation), 5, 10); // Rotate the image..rotate(Math.toRadians(angle), m_imageWidth/2, m_imageHeight/2);
+        g2d.drawImage(p.img, 0, 0, 10, 20, j);
+        
+        g2d.setTransform(old);
+        for(bullet b : bullets){
+        g2d.drawImage(b.img, (int)b.xPos, (int)b.yPos, 10, 10, j);
+        }
+    }
+
+    public static void main(String[] args) {
+        j = new JetMovement();
+    }
 
 }
